11fe0477d02b2962c15a24ad800115a6dbe64961
hexin <398229299@qq.com>
2020-01-15 13:25:24 +0000

    命名路由

--- public/js/app.js
+++ public/js/app.js
@@ -37136,15 +37136,27 @@ window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 
 /***/ }),
 
+/***/ "./resources/sass/layout.scss":
+/*!************************************!*\
+  !*** ./resources/sass/layout.scss ***!
+  \************************************/
+/*! no static exports found */
+/***/ (function(module, exports) {
+
+// removed by extract-text-webpack-plugin
+
+/***/ }),
+
 /***/ 0:
-/*!*************************************************************!*\
-  !*** multi ./resources/js/app.js ./resources/sass/app.scss ***!
-  \*************************************************************/
+/*!******************************************************************************************!*\
+  !*** multi ./resources/js/app.js ./resources/sass/app.scss ./resources/sass/layout.scss ***!
+  \******************************************************************************************/
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
 __webpack_require__(/*! /home/vagrant/Code/Weibo/resources/js/app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! /home/vagrant/Code/Weibo/resources/sass/app.scss */"./resources/sass/app.scss");
+__webpack_require__(/*! /home/vagrant/Code/Weibo/resources/sass/app.scss */"./resources/sass/app.scss");
+module.exports = __webpack_require__(/*! /home/vagrant/Code/Weibo/resources/sass/layout.scss */"./resources/sass/layout.scss");
 
 
 /***/ })
--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -3,6 +3,7 @@
 <head>
     <title>@yield('title', 'MicroBlog') - Laravel 入门教程</title>
     <link rel="stylesheet" href="{{ mix('css/app.css') }}">
+    <link rel="stylesheet" href="{{ mix('css/layout.css') }}">
 </head>
 <body>
 
