ad89ea76d8dcb9249cf2ac647651432c9656a468
hexin <398229299@qq.com>
2020-01-16 15:34:25 +0000

    用户显示页面

--- routes/web.php
+++ routes/web.php
@@ -6,3 +6,4 @@ Route::get('/help', 'StaticPagesController@help')->name('help');
 Route::get('/about', 'StaticPagesController@about')->name('about');
 
 Route::get('register', 'UsersController@create')->name('register');
+Route::resource('users', 'UsersController');
