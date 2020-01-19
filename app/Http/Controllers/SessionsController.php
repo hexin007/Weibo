a13ac9843591d377b4f2ba0ca69b0bd274035f15
hexin <398229299@qq.com>
2020-01-19 14:49:00 +0000

    用户退出登陆

--- app/Http/Controllers/SessionsController.php
+++ app/Http/Controllers/SessionsController.php
@@ -33,4 +33,11 @@ class SessionsController extends Controller
             return redirect()->back()->withInput();
         }
     }
+
+    public function destroy()
+    {
+        Auth::logout();
+        session()->flash('success', '您已成功退出！');
+        return redirect()->route('login');
+    }
 }
